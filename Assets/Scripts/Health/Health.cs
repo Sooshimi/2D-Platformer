65b6d8f4e5fde3087ec757b2174e9390d7d76119
Sooshimi <masonchee93@gmail.com>
2022-09-03 12:23:05 +0100

    Added iFrames

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,16 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField]private float startingHealth;
+    [Header ("Health")]
+    [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; } // can get var from other scripts, but can only set within this script
     private Animator anim;
     private bool dead;
+
+    [Header ("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend; // grabs ref to SpriteRenderer component
 
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -21,7 +29,9 @@ public class Health : MonoBehaviour
         {
             // Player hurt
             anim.SetTrigger("hurt");
+
             // iframes
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -39,5 +49,18 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true); // 10 and 11 are the player and enemy layers, respectively
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false); 
     }
 }
