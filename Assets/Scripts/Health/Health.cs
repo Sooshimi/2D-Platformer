116c899b1a47ead03c2f4471513fe22a9ac05679
Sooshimi <masonchee93@gmail.com>
2022-09-04 15:59:15 +0100

    Added new traps

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -53,7 +53,7 @@ public class Health : MonoBehaviour
 
     private IEnumerator Invulnerability()
     {
-        Physics2D.IgnoreLayerCollision(10, 11, true); // 10 and 11 are the player and enemy layers, respectively
+        Physics2D.IgnoreLayerCollision(8, 9, true); // 8 and 9 are the player and enemy layers, respectively
         for (int i = 0; i < numberOfFlashes; i++)
         {
             spriteRend.color = new Color(1, 0, 0, 0.5f);
@@ -61,6 +61,6 @@ public class Health : MonoBehaviour
             spriteRend.color = Color.white;
             yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
         }
-        Physics2D.IgnoreLayerCollision(10, 11, false); 
+        Physics2D.IgnoreLayerCollision(8, 9, false); 
     }
 }
